1c527257d485ec8b04421689e0bfae1eba6488fd
Mega-vardhini <megas27122007@gmail.com>
2026-06-07 16:53:09 +0530

    Add files via upload

---
+++ hangman_game.py
@@ -0,0 +1,42 @@
+import random
+
+words = ["python", "apple", "computer", "program", "school"]
+word = random.choice(words)
+
+guessed_letters = []
+wrong_guesses = 0
+max_wrong = 6
+
+display = ["_"] * len(word)
+
+print("Welcome to Hangman!")
+
+while wrong_guesses < max_wrong and "_" in display:
+    print("\nWord:", " ".join(display))
+    print("Wrong guesses left:", max_wrong - wrong_guesses)
+
+    guess = input("Enter a letter: ").lower()
+
+    if len(guess) != 1 or not guess.isalpha():
+        print("Please enter a single letter.")
+        continue
+
+    if guess in guessed_letters:
+        print("You already guessed that letter.")
+        continue
+
+    guessed_letters.append(guess)
+
+    if guess in word:
+        for i in range(len(word)):
+            if word[i] == guess:
+                display[i] = guess
+        print("Correct!")
+    else:
+        wrong_guesses += 1
+        print("Wrong!")
+
+if "_" not in display:
+    print("\nCongratulations! You guessed the word:", word)
+else:
+    print("\nGame Over! The word was:", word)
